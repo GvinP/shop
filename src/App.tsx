d15dc43f2473a5c6c3b83707a83fcd79a34b650e
GvinP <gvinpin72@gmail.com>
2022-09-02 11:09:04 +0400

    add productlist page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import { Main } from './pages/Main';
+import { Announcement } from "./components/Announcement";
+import { Navbar } from "./components/Navbar";
+import { Main } from "./pages/Main";
+import { ProductList } from "./pages/ProductList";
+import { Footer } from "./components/Footer";
+import { Newsletter } from "./components/Newsletter";
+import { Products } from "./components/Products";
 
 function App() {
   return (
-    <Main/>
+    <>
+      <Announcement />
+      <Navbar />
+      <ProductList />
+      <Products />
+      <Newsletter />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/pages/ProductList.tsx
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+const Title = styled.h1`
+  margin: 20px;
+`;
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const Filter = styled.div`
+  margin: 20px;
+`;
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+`;
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+`;
+const Option = styled.option``;
+
+export const ProductList = () => {
+  return (
+    <Container>
+      <Title>WOMEN'S TOPS</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filter products: </FilterText>
+          <Select>
+            <Option selected disabled>
+              Color
+            </Option>
+            <Option>White</Option>
+            <Option>Black</Option>
+            <Option>Red</Option>
+            <Option>Blue</Option>
+            <Option>Yellow</Option>
+            <Option>Green</Option>
+          </Select>
+          <Select>
+            <Option selected disabled>
+              Size
+            </Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort products: </FilterText>
+          <Select>
+            <Option selected>Newest</Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (desc)</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+    </Container>
+  );
+};
